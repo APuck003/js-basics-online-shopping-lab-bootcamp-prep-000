5f02ae92d0d4307c6fd1401327d2b65ba31f34a7
Austin Puckett <puckett_austin@yahoo.com>
2018-09-23 02:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function viewCart() {
     return twoItems;
   } else {
     var cartData = [];
-    for (var i = 0; i < cart.length-1; i++){
+    for (var i = 0; i < getCart.length-1; i++){
       cartData.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`);
 
     }
