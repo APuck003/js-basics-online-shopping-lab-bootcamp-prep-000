5e6de7da5c0c8d630d1452df6cdecb90e0025f97
Austin Puckett <puckett_austin@yahoo.com>
2018-09-23 01:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,14 +53,13 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length === 0){
-    // console.log('Your shopping cart is empty.');
     return 'Your shopping cart is empty.';
   } else if (cart.length === 1){
     var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`;
-    console.log(oneItem);
+    return oneItem;
   } else if (cart.length === 2){
     var twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`;
-    console.log(twoItems);
+    return twoItems;
   } else {
     var cartData = [];
     for (var i = 0; i < cart.length-1; i++){
