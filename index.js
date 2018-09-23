de30c6d86c12e81ff8aa3ad259c2343ebe974d8a
Austin Puckett <puckett_austin@yahoo.com>
2018-09-23 02:55:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,11 +81,11 @@ function viewCart() {
   }
   
   if (cart.length === 0){
-    console.log(`Your shopping cart is empty.`);
+    return `Your shopping cart is empty.`;
   } else if (cart.length <= 2){
-    console.log(`In your cart, you have ${cartArray.join(' and ')}.`);
+    return `In your cart, you have ${cartArray.join(' and ')}.`;
   } else if (cart.length >= 3){
-    console.log(`In your cart, you have ${cartArray.slice(0, cartArray.length - 1).join(', ')}, and ${cartArray.slice(-1)}.`);
+    return `In your cart, you have ${cartArray.slice(0, cartArray.length - 1).join(', ')}, and ${cartArray.slice(-1)}.`;
   }
 }
 
