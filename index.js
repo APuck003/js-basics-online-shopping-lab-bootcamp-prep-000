cce51f067683681b868b21b35b44b82baaa85e7d
Austin Puckett <puckett_austin@yahoo.com>
2018-09-23 02:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function viewCart() {
 
     }
     var threePlusItems = `In your cart, you have ${cartData.join(', ')}, and ${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`;
-    console.log(threePlusItems);
+    return threePlusItems;
   }
 }
 
