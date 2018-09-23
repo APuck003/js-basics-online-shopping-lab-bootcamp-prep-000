69d3fe1c2bb3a0e2db5a9f2dd92b8c4aa7477c84
Austin Puckett <puckett_austin@yahoo.com>
2018-09-23 01:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,28 +28,30 @@ function addToCart(item) {
 
 
 
-function viewCart() {
-  if (cart.length === 0) {
-    // console.log("Your shopping cart is empty.");
-    return "Your shopping cart is empty.";
-  } else if (cart.length === 1) {
-    var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`;
-    // console.log(oneItem);
-    return oneItem;
-  } else if (cart.length === 2) {
-    var twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}, and ${Object.keys(cart[1])} at ${Object.values(cart[1])}.`;
-    // console.log(twoItems);
-    return twoItems;
-  } else {
-    var cartArray = [];
+// function viewCart() {
+//   if (cart.length === 0) {
+//     // console.log("Your shopping cart is empty.");
+//     return "Your shopping cart is empty.";
+//   } else if (cart.length === 1) {
+//     var oneItem = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`;
+//     // console.log(oneItem);
+//     return oneItem;
+//   } else if (cart.length === 2) {
+//     var twoItems = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}, and ${Object.keys(cart[1])} at ${Object.values(cart[1])}.`;
+//     // console.log(twoItems);
+//     return twoItems;
+//   } else {
+//     var cartArray = [];
     
-    for (var i = 0; i < cart.length - 1; i++) {
-      cartArray.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`);
-    }
-    var threeOrMore = `In your cart, you have ${cartArray.join(', ')}, and ${Object.keys(cart[cart.length - 1])} at $${Object.values(cart[cart.length - 1])}.`;
-    console.log(threeOrMore);
-  }
-}
+//     for (var i = 0; i < cart.length - 1; i++) {
+//       cartArray.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`);
+//     }
+//     var threeOrMore = `In your cart, you have ${cartArray.join(', ')}, and ${Object.keys(cart[cart.length - 1])} at $${Object.values(cart[cart.length - 1])}.`;
+//     console.log(threeOrMore);
+//   }
+// }
+
+
 
 function total() {
   // write your code here
